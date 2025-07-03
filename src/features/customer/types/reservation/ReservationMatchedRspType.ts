d1ad2b27ed1096f265d8b337463151267abbe76c
sujeong <suujeong0@gmail.com>
2025-07-03 16:27:37 +0900

    refactor: 기존 나눠져 있던 reservationType 삭제

--- src/features/customer/types/reservation/ReservationMatchedRspType.ts
+++ src/features/customer/types/reservation/ReservationMatchedRspType.ts
@@ -1,5 +1,4 @@
-import type { ReservationRspType } from '@/features/customer/types/reservation/ReservationRspType';
-import type { ServiceCategoryTreeType } from '@/features/customer/types/reservation/ServiceCategoryTreeType';
+
 import type { ManagerMatchingRspType } from '@/features/customer/types/CustomerReservationType';
 
 export interface ReservationMatchedRspType {
