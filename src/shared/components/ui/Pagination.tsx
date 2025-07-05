858bc752a3246d6accd8f438e87e55bafcaab79c
SighingOwl <whddms1208@gmail.com>
2025-07-06 05:28:13 +0900

    refactor: 공통 컴포넌트 ui 리펙토링

--- src/shared/components/ui/Pagination.tsx
+++ src/shared/components/ui/Pagination.tsx
@@ -1,54 +1,60 @@
-import React from "react";
+import React from 'react'
 
 interface PaginationProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
 }
 export const Pagination = ({
   children,
-  className = "",
+  className = '',
   ...rest
 }: PaginationProps) => (
-  <div className={className} {...rest}>
+  <div
+    className={className}
+    {...rest}>
     {children}
   </div>
-);
+)
 
 interface PaginationContentProps extends React.HTMLAttributes<HTMLDivElement> {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
 }
 export const PaginationContent = ({
   children,
-  className = "",
+  className = '',
   ...rest
 }: PaginationContentProps) => (
-  <div className={className} {...rest}>
+  <div
+    className={className}
+    {...rest}>
     {children}
   </div>
-);
+)
 
 interface PaginationItemProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 export const PaginationItem = ({ children }: PaginationItemProps) => (
   <span>{children}</span>
-);
+)
 
 interface PaginationLinkProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  className?: string;
-  children: React.ReactNode;
+  className?: string
+  children: React.ReactNode
 }
 export const PaginationLink = ({
   children,
-  className = "",
+  className = '',
   ...rest
 }: PaginationLinkProps) => (
-  <a className={className} {...rest}>
+  <a
+    className={className}
+    {...rest}>
     {children}
   </a>
-);
+)
 
-export const PaginationNext = PaginationLink;
-export const PaginationPrevious = PaginationLink;
+export const PaginationNext = PaginationLink
+export const PaginationPrevious = PaginationLink
